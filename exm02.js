486f38a20cb7dc5be200abd322b97c6a336a704e
Joel Henrique <joel.henrique@estudante.ufjf.br>
2022-06-28 19:47:39 -0300

    adiciona tipos simples

--- exm02.js
+++ exm02.js
@@ -1,1 +1,12 @@
 var a = 10;
+console.log(typeof a);
+var b = "isso eh um texto";
+console.log(typeof b);
+var c = true;
+console.log(typeof c);
+var d;
+console.log(typeof d);
+var e = null;
+console.log(typeof e);
+var f = function(){};
+console.log(typeof f);
