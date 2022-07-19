30b825d4f372db786f3668951e4296f30bac9417
Joel Henrique <joel.henrique@estudante.ufjf.br>
2022-07-19 20:56:27 -0300

    cria estado fixo

--- form01.js
+++ form01.js
@@ -1,6 +1,8 @@
-let estado_cor;
-let estado_altura;
-let estado_largura;
+let estado = {
+    cor: "#000000",
+    altura: 0,
+    largura: 0,
+}
 
 function criaElemento(cor, altura, largura){
     const eNovo = document.createElement("div");
@@ -11,10 +13,10 @@ function criaElemento(cor, altura, largura){
 document.exm01.addEventListener("submit",//submitListener
 (event) => { 
     event.preventDefault();
-    estado_cor = document.exm01.cor.value,
-    estado_altura = Number(document.exm01.altura.value),
-    estado_largura = Number(document.exm01.largura.value)
-    criaElemento(estado_cor,estado_altura,estado_largura);
+    estado.cor = document.exm01.cor.value,
+    estado.altura = Number(document.exm01.altura.value),
+    estado.largura = Number(document.exm01.largura.value)
+    criaElemento(estado.cor,estado.altura,estado.largura);
     
     //+document.exm01.altura.value,
     //+document.exm01.largura.value,
