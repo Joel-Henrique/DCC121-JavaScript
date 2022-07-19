112894bec79cc51acc98c7aea33b42e64dd129e8
Joel Henrique <joel.henrique@estudante.ufjf.br>
2022-07-19 20:32:55 -0300

    adiciona reset apos submit

--- form01.js
+++ form01.js
@@ -1,15 +1,24 @@
-function criaElemento(cor, largura, altura){
+function criaElemento(cor, altura, largura){
     const eNovo = document.createElement("div");
-    eNovo.textContent = `${cor} ${largura} ${altura}`;
+    eNovo.textContent = `${cor} ${altura} ${largura}`;
     document.body.appendChild(eNovo);
 }
-document.exm01.addEventListener("submit",submitListener);
-
-function submitListener(event){ 
+document.exm01.addEventListener("submit",//submitListener
+(event) => { 
     event.preventDefault();
     criaElemento(
     document.exm01.cor.value,
     document.exm01.largura.value,
     document.exm01.altura.value,
 );
-}
+    document.exm01.rest();
+});
+
+/* function submitListener(event){ 
+    event.preventDefault();
+    criaElemento(
+    document.exm01.cor.value,
+    document.exm01.largura.value,
+    document.exm01.altura.value,
+);
+} */
