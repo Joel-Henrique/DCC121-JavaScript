89b82ec1a4ff5f7863252e7d4833b3e5b908f090
Joel Henrique <joel.henrique@estudante.ufjf.br>
2022-07-19 20:25:52 -0300

    arruma botão de submit

--- form01.js
+++ form01.js
@@ -3,8 +3,13 @@ function criaElemento(cor, largura, altura){
     eNovo.textContent = `${cor} ${largura} ${altura}`;
     document.body.appendChild(eNovo);
 }
-criaElemento(
+document.exm01.addEventListener("submit",submitListener);
+
+function submitListener(event){ 
+    event.preventDefault();
+    criaElemento(
     document.exm01.cor.value,
     document.exm01.largura.value,
     document.exm01.altura.value,
 );
+}
